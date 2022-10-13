3051432a77243b785518cffec6d495632cb41daf
huy03 <huydao1008@gmail.com>
2022-10-13 19:35:26 +0700

    edit

--- sample/obj/Debug/net6.0-windows/App.g.i.cs
+++ sample/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace sample {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace sample {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.3.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public static void Main() {
             sample.App app = new sample.App();
             app.InitializeComponent();
